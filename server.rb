fbc99314ce5e760afb3af8b5a5b3338038f55544
AbhishekKr <abhikumar163@gmail.com>
2013-08-13 02:22:37 +0530

    made to run at thin

--- server.rb
+++ server.rb
@@ -4,17 +4,19 @@ require 'haml'
 $localdir = ENV['STASH'] || "cut_out__espionage"
 Dir.mkdir $localdir unless File.exists? $localdir
 
-get '/' do
-  redirect '/stash'
-end
+class DeadDrop < Sinatra::Base
+  get '/' do
+    redirect '/stash'
+  end
 
-get '/stash' do
-  haml :stash
-end
+  get '/stash' do
+    haml :stash
+  end
 
-post '/stash' do
-  filename = params['dead_body'][:filename]
-  tempfile = params['dead_body'][:tempfile]
-  File.write("#{$localdir}/#{filename}", tempfile.read)
-  return "#{filename} was successfully uploaded!"
+  post '/stash' do
+    filename = params['dead_body'][:filename]
+    tempfile = params['dead_body'][:tempfile]
+    File.write("#{$localdir}/#{filename}", tempfile.read)
+    return "#{filename} was successfully uploaded!"
+  end
 end
