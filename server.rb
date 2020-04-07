2dd13a9e1bbe2edca8c790678f84a18a8b8d65a0
AbhishekKr <abhikumar163@gmail.com>
2020-04-07 11:39:23 +0530

    add /stash/:filename to fetch from stash, 4xx handling for file to stash
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- server.rb
+++ server.rb
@@ -1,8 +1,11 @@
 require 'sinatra'
 require 'haml'
+require 'securerandom'
+
 
 $localdir = ENV['STASH'] || "cut_out__espionage"
 Dir.mkdir $localdir unless File.exists? $localdir
+
 
 class DeadDrop < Sinatra::Base
   get '/' do
@@ -14,10 +17,34 @@ class DeadDrop < Sinatra::Base
   end
 
   post '/stash' do
-    filename = params['dead_body'][:filename]
-    tempfile = params['dead_body'][:tempfile]
-    File.write("#{$localdir}/#{filename}", tempfile.read)
-    return "#{filename} was successfully uploaded!"
+    begin
+      filename = params['dead_body'][:filename]
+      tempfile = params['dead_body'][:tempfile]
+      new_filename = "#{SecureRandom.uuid}-#{filename}"
+      localfilepath = File.join($localdir, new_filename)
+      File.write(localfilepath, tempfile.read)
+      return "
+      <b>Congrats!</b><br/>
+      #{filename} was successfully uploaded.<br/>
+      Available at path <b>https://<my-domain>/stash/#{new_filename}</b>"
+    rescue
+      return "
+      <b>Error 400</b><br/>
+      there were some issues,<br/>try again in some time
+      "
+    end
+  end
+
+  get '/stash/:filename' do |filename|
+    localfilepath = File.join($localdir, filename)
+    File.expand_path(localfilepath)
+    unless File.file?(localfilepath) and localfilepath.include?($localdir) then
+      return "
+      <b>Error 404</b><br/>
+      <blockquote>Oh <i>#{filename}</i>,<br/> Where Art Thou!</blockquote>
+      "
+    end
+    send_file localfilepath
   end
 
   get '/ping' do
